ff3d55025a031834074eb502587c592a96a8199c
yusufcanarslan <yusuf.can.arslan.96@gmail.com>
2022-01-24 12:05:53 +0300

    Add files via upload

---
+++ PrepaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.prepaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
